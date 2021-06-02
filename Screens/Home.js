e387d158ab3d5a7ce0b015868e2d6b1bbd1b8ebf
deriksadiki <deriksadiki@gmail.com>
2021-06-02 13:27:16 +0200

    today

--- Screens/Home.js
+++ Screens/Home.js
@@ -9,9 +9,9 @@ export default class Home extends React.Component{
       <View>
         <Text>hello Home page</Text>
         <TouchableOpacity onPress={()=>this.props.navigation.navigate('History')}>
-            <Text>go to past</Text>
+            <Text>go to past.</Text>
         </TouchableOpacity>
       </View>
     )
   }
-}
+}  
